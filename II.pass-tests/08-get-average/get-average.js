ad719389353ed96c083eeb62b1af141b769e7fb9
Maaly <maalykaraki@yahoo.com>
2019-01-07 22:58:07 +0000

    get average of array

--- II.pass-tests/08-get-average/get-average.js
+++ II.pass-tests/08-get-average/get-average.js
@@ -1,3 +1,9 @@
 // the input is an array of numbers and strings
 // return the average of all the numbers
 // be sure to exclude the strings
+
+function getAverage(element) {
+  var arr = element.filter(number => typeof number === "number");
+  return arr.reduce((a, b) => a + b) / arr.length;
+}
+module.exports = getAverage;
